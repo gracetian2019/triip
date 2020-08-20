4aa918cd9ba9cb2545db3b2e47b76f93a1f8f2fd
gracetian2019 <gracetian4ever@gmail.com>
2020-08-19 21:37:49 -0400

    backend added serializer gem added comments seed changed table

--- triip/db/migrate/20200817223226_create_comments.rb
+++ triip/db/migrate/20200817223226_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
       t.string :content
-      t.integer :destination_id
+      t.references :destination, foreign_key: true
       t.timestamps
     end
   end
